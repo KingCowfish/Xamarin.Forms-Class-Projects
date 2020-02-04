bb0f3673c0334216e5683083035b1530e7f2b1c3
Alexander Broder <brodera146@gmail.com>
2020-02-04 13:49:42 -0800

    Update.

---
+++ HelloWorld/HelloWorld/HelloWorld/ImagePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagePage : ContentPage
+    {
+        public ImagePage()
+        {
+            InitializeComponent();
+
+            image.Source = ImageSource.FromResource("HelloWorld.Images.background.jpg");
+
+            //Downloaded Images:
+
+            //var imageSource = new UriImageSource { Uri = new Uri("https://i.ytimg.com/vi/2jIZ3H9k2rA/maxresdefault.jpg") };
+            //imageSource.CachingEnabled = false;
+            //image.Source = imageSource;
+
+            //Embedded Images
+
+
+        }
+    }
+}
